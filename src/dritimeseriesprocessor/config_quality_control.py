baa6620cfabd548088e73595370a2e3d258296a6
Lewis Chambers <lewcha@ceh.ac.uk>
2024-08-09 16:27:44 +0100

    Added QC test validator with tests

--- src/dritimeseriesprocessor/config_quality_control.py
+++ src/dritimeseriesprocessor/config_quality_control.py
@@ -343,3 +343,83 @@ def get_qc_flag(test_names: List[int]) -> int:
             raise KeyError(f'Test name "{name}" not found in valid QC tests.')
 
     return flag
+
+
+class QCTestIDValidator:
+    """Validates that a list of QC tests is valid and non-wasteful
+
+    These methods ensure that the test_id values are unique, bitwise,
+    sequential, and don't skip any valid bits.
+
+    It is desirable to not waste any bits, because maximum bits can grow
+    quite large."""
+
+    @staticmethod
+    def _ids_are_unique(test_list: List[dict]) -> bool:
+        """Checks if IDs are unique
+
+        Args:
+            test_list: A list of dictionaries representing QC tests.
+
+        Returns:
+            bool: A bool result of whether the IDs are unique.
+        """
+
+        test_ids = [test["test_id"] for test in test_list]
+
+        if len(test_ids) == len(set(test_ids)):
+            return True
+
+        return False
+
+    @staticmethod
+    def _ids_are_sequential(test_list: List[dict]) -> bool:
+        """Checks that IDs are sequential and start at number 1.
+
+        Args:
+            test_list: A list of dictionaries representing QC tests.
+
+        Returns:
+            bool: A bool result of whether the IDs are sequential and start at 1.
+        """
+
+        for i, test in enumerate(test_list):
+            if test["test_id"] != 1 << i:
+                return False
+
+        return True
+
+    @staticmethod
+    def _ids_are_bitwise(test_list: List[dict]) -> bool:
+        """Checks that IDs are sequential and start at number 1.
+
+        Args:
+            test_list: A list of dictionaries representing QC tests.
+
+        Returns:
+            bool: A bool result of whether the IDs are bitwise.
+        """
+        for test in test_list:
+            if test["test_id"] == 0 or ((test["test_id"] & (test["test_id"] - 1)) != 0):
+                return False
+
+        return True
+
+    @staticmethod
+    def validate(test_list: List[dict]) -> bool:
+        """Checks that IDs in a list of tests are valid.
+
+        Args:
+            test_list: A list of dictionaries representing QC tests.
+
+        Returns:
+            bool: A bool result of whether the IDs are valid.
+        """
+
+        return all(
+            [
+                QCTestIDValidator._ids_are_unique(test_list),
+                QCTestIDValidator._ids_are_bitwise(test_list),
+                QCTestIDValidator._ids_are_sequential(test_list),
+            ]
+        )
--- tests/test_config_quality_control.py
+++ tests/test_config_quality_control.py
@@ -12,7 +12,8 @@ from dritimeseriesprocessor.config_quality_control import (
     QCConfig,
     get_valid_qc_tests,
     get_qc_config,
-    get_qc_flag
+    get_qc_flag,
+    QCTestIDValidator
 )
 
 class TestGetValidQCTests(unittest.TestCase):
@@ -276,6 +277,109 @@ class TestQCFlagging(unittest.TestCase):
         flag = get_qc_flag(test_names)
 
         self.assertEqual(flag, expected)
+
+class TestQCTestValidity(unittest.TestCase):
+    """Suite to check validity of QC tests in codebase"""
+
+    def setUp(self):
+        self.good_tests = [
+            {
+                "test_name": "RANGE",
+                "test_id": 1 << 0
+            },
+            {
+                "test_name": "MIN",
+                "test_id": 1 << 1
+            },
+            {
+                "test_name": "MAX",
+                "test_id": 1 << 2
+            },
+        ]
+
+    def test_unique_ids(self):
+        """Ensures that all tests have unique IDs"""
+
+        self.assertTrue(
+            QCTestIDValidator._ids_are_unique(self.good_tests)
+        )
+
+        non_unique_ids = [
+            {
+                "test_name": "RANGE",
+                "test_id": 1 << 0
+            },
+            {
+                "test_name": "MIN",
+                "test_id": 1 << 0
+            },
+            {
+                "test_name": "MAX",
+                "test_id": 1 << 2
+            },
+        ]
+
+        self.assertFalse(QCTestIDValidator._ids_are_unique(non_unique_ids))
+    
+    def test_bitwise_ids(self):
+        """Ensures that all test_ids are bitwise (2**n)"""
+
+        self.assertTrue(
+            QCTestIDValidator._ids_are_bitwise(self.good_tests)
+        )
+
+        bad_tests = [
+            {
+                "test_name": "RANGE",
+                "test_id": 1 << 0
+            },
+            {
+                "test_name": "MIN",
+                "test_id": 7
+            },
+            {
+                "test_name": "MAX",
+                "test_id": 1.6
+            },
+        ]
+
+        self.assertFalse(QCTestIDValidator._ids_are_bitwise(bad_tests))
+    
+    def test_sequential_ids(self):
+        """Ensures that all tests have sequential IDs"""
+
+        self.assertTrue(
+            QCTestIDValidator._ids_are_sequential(self.good_tests)
+        )
+
+        bad_tests = [
+            {
+                "test_name": "RANGE",
+                "test_id": 1 << 1
+            },
+            {
+                "test_name": "MIN",
+                "test_id": 1 << 2
+            },
+            {
+                "test_name": "MAX",
+                "test_id": 1 << 0
+            },
+        ]
+
+        self.assertFalse(QCTestIDValidator._ids_are_sequential(bad_tests))
+
+    @patch("dritimeseriesprocessor.config_quality_control.QCTestIDValidator._ids_are_sequential")
+    @patch("dritimeseriesprocessor.config_quality_control.QCTestIDValidator._ids_are_bitwise")
+    @patch("dritimeseriesprocessor.config_quality_control.QCTestIDValidator._ids_are_unique")
+    def test_validation_methods_called(self, mock_ids_are_unique, mock_ids_are_bitwise, mock_ids_are_sequential):
+        """Tests that all validation methods are called when main function invoked"""
+
+        assert QCTestIDValidator.validate(self.good_tests)
+        assert mock_ids_are_unique.called
+        assert mock_ids_are_bitwise.called
+        assert mock_ids_are_sequential.called
+
 
 
 if __name__ == '__main__':
