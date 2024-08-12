2daadb1dbd6f20114b056b92727992e98c00a08d
ricsmi <ricsmi@ceh.ac.uk>
2024-08-12 16:43:25 +0100

    Ruff format linting

--- src/dritimeseriesprocessor/__main__.py
+++ src/dritimeseriesprocessor/__main__.py
@@ -12,7 +12,9 @@ logger = logging.getLogger(__name__)
 data_category = "PRECIP_1MIN_2024_LOOPED"
 start_date = date(2024, 1, 17)
 end_date = date(2024, 1, 17)
-data = data_manager.query_by_date_range(app_config.level_0_bucket, data_category, start_date, end_date, site_ids="BUNNY")
+data = data_manager.query_by_date_range(
+    app_config.level_0_bucket, data_category, start_date, end_date, site_ids="BUNNY"
+)
 
 logger.info(data.count())
 logger.info(data)
