6d89c3ff6722bef5bf486751a4adc03b31b84d58
ricsmi <ricsmi@ceh.ac.uk>
2024-08-13 11:42:23 +0100

    Updated bucket 'data category' to instead be 'prefix', to handle project level directory

--- src/dritimeseriesprocessor/__main__.py
+++ src/dritimeseriesprocessor/__main__.py
@@ -9,11 +9,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Get data
-data_category = "PRECIP_1MIN_2024_LOOPED"
-start_date = date(2024, 1, 17)
-end_date = date(2024, 1, 17)
+prefix = "cosmos/PRECIP_1MIN_2024_LOOPED"
+start_date = date(2024, 1, 30)
+end_date = date(2024, 2, 2)
 data = data_manager.query_by_date_range(
-    app_config.level_0_bucket, data_category, start_date, end_date, site_ids="BUNNY"
+    app_config.level_0_bucket, prefix, start_date, end_date, site_ids="BUNNY"
 )
 
 logger.info(data.count())
