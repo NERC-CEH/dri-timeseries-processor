2fddba58d48e112e7ed0e8b5e6139053a701b402
natsha <natsha@ceh.ac.uk>
2024-08-15 16:43:41 +0100

    setup error codes qc test

--- src/dritimeseriesprocessor/__metadata__/config_quality_control.py
+++ src/dritimeseriesprocessor/__metadata__/config_quality_control.py
@@ -65,6 +65,30 @@ qc_tests = {
             "WS",
         ],
     },
+    "ERROR_CODES": {
+        "test_name": "Error codes qc check",
+        "description": "Check for specific error codes",
+        "variables": [
+            "WS",
+            "COV_TS_UY",
+            "CTS_MOD",
+            "G2",
+            "HS",
+            "LWOUT",
+            "RHO_A_MEAN",
+            "SWOUT",
+            "TDT4_TSOIL",
+            "TDT4_VWC",
+            "TDT5_SOILEC",
+            "TDT7_VWC",
+            "TDT8_VWC",
+            "TDT9_SOILEC",
+            "COV_UX_UY",
+            "LWIN",
+            "PROFILE_SOILEC15",
+            "PROFILE_VWC15"
+        ]
+    }
 }
 
 # Range test - Min and max values for variables.
