98a2d41885ce55d1d8a3bc044a59c66974cb71c7
Lewis Chambers <lewcha@ceh.ac.uk>
2024-09-26 15:28:44 +0100

    Added cosmos data and made holes

--- bin/build-gapped-dataset.py
+++ bin/build-gapped-dataset.py
@@ -21,11 +21,14 @@ COSMOS_GAP_DIR = DATA_DIR / "cosmos-with-gaps"
 
 # Creating the directory and copying files
 
-initialise_directory(COSMOS_GAP_DIR / "PRECIP_1MIN_2024_LOOPED", COSMOS_DIR / "PRECIP_1MIN_2024_LOOPED", purge=True)
+initialise_directory(COSMOS_GAP_DIR, COSMOS_DIR, purge=True)
 
 # Removing data
 
 COSMOS_GAP_PRECIP_DIR = COSMOS_GAP_DIR / "PRECIP_1MIN_2024_LOOPED"
+COSMOS_GAP_SOILMET_DIR = COSMOS_GAP_DIR / "SOILMET_30MIN_2024_LOOPED"
+
+##### PRECIPITATION #####
 
 # Rows and cells removed
 builder = ParquetBuilder(COSMOS_GAP_PRECIP_DIR / "2024-01" / "2024-01-17.parquet")
@@ -82,4 +85,43 @@ builder.reset()
 builder.target = COSMOS_GAP_PRECIP_DIR / "2024-02" / "2024-02-04.parquet"
 builder.output = builder.target
 builder.build_all(row_removal_percent=70, cell_removal_percent=60, protected_columns=protected_cols)
+builder.write_output()
+
+##### SOILMET ####
+
+# Remove a full day
+os.remove(COSMOS_GAP_SOILMET_DIR / "2024-02" / "2024-02-25.parquet")
+
+# Making very little and gappy data
+builder.reset()
+builder.target = COSMOS_GAP_SOILMET_DIR / "2024-02" / "2024-02-24.parquet"
+builder.output = builder.target
+builder.build_all(row_removal_percent=70, cell_removal_percent=60, protected_columns=protected_cols)
+builder.write_output()
+
+# Midnight crossing gap
+builder.reset()
+builder.target = COSMOS_GAP_SOILMET_DIR / "2024-02" / "2024-02-26.parquet"
+builder.output = builder.target
+builder.build_all(clear_after_time=time(hour=20))
+builder.write_output()
+
+builder.reset()
+builder.target = COSMOS_GAP_SOILMET_DIR / "2024-02" / "2024-02-27.parquet"
+builder.output = builder.target
+builder.build_all(clear_before_time=time(hour=2))
+builder.write_output()
+
+# High corruption
+builder.reset()
+builder.target = COSMOS_GAP_SOILMET_DIR / "2024-02" / "2024-02-28.parquet"
+builder.output = builder.target
+builder.build_all(cell_removal_percent=75, protected_columns=protected_cols)
+builder.write_output()
+
+# Rows and cells removed
+builder.reset()
+builder.target = COSMOS_GAP_SOILMET_DIR / "2024-02" / "2024-02-29.parquet"
+builder.output = builder.target
+builder.build_all(row_removal_percent=30, cell_removal_percent=10, protected_columns=protected_cols)
 builder.write_output()
