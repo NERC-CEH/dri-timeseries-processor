59605c934544dab3267b0da24b692aed3b9331f5
natsha <natsha@ceh.ac.uk>
2024-10-17 10:00:53 +0100

    add newly partitioned data

--- src/dritimeseriesprocessor/__main__.py
+++ src/dritimeseriesprocessor/__main__.py
@@ -28,8 +28,8 @@ try:
 
     # Get data
     prefix = "cosmos-with-gaps/dataset=PRECIP_1MIN_2024_LOOPED"
-    start_date = date(2024, 1, 30)
-    end_date = date(2024, 2, 1)
+    start_date = date(2024, 3, 28)
+    end_date = date(2024, 3, 29)
     data = data_manager.query_by_date_range(
         app_config.level_0_bucket,
         prefix,
