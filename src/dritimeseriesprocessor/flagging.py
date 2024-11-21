2d7f76c2f28efd35dfb6ee2ab1bc84856418b1ad
Simon Stanley <simon.stanley87@gmail.com>
2024-11-21 15:09:09 +0000

    Update init flagging with immutability

--- src/dritimeseriesprocessor/flagging.py
+++ src/dritimeseriesprocessor/flagging.py
@@ -22,17 +22,41 @@ def initialise_core_flags(ts: TimeSeries) -> TimeSeries:
     Returns:
         The TimeSeries with the flag columns added
     """
+    flag_col_dict = {}
     for data_col_name in ts.data_col_names:
-        flag_col_name = f"{data_col_name}_FLAG"
-        ts.add_supp_column(flag_col_name, 0)
+        flag_col_dict[data_col_name] = f"{data_col_name}_FLAG"
 
-        ts._df = add_unchecked_flag(ts.df, flag_col_name)
-        ts._df = add_missing_flag(ts.df, data_col_name, flag_col_name)
+    flag_col_names = flag_col_dict.values()
+
+    # Add flag columns to TimeSeries as supplementary columns
+    ts = ts.df_operation(add_flag_columns, flag_col_names=flag_col_names)
+    ts = ts.set_columns_supplemental(flag_col_names)
+
+    # Add initial flags
+    ts = ts.df_operation(add_unchecked_flag, flag_col_names=flag_col_names)
+    ts = ts.df_operation(add_missing_flag, flag_col_dict=flag_col_dict)
 
     return ts
 
 
-def add_unchecked_flag(df: pl.DataFrame, flag_col_name: str) -> pl.DataFrame:
+def add_flag_columns(df: pl.DataFrame, flag_col_names: list) -> pl.DataFrame:
+    """
+    Add flag columns to dataframe for each given column, initialising as 0.
+
+    Args:
+        df: The dataframe to update
+        flag_col_name: Name of flag column
+
+    Returns:
+        Updated dataframe
+    """
+    for flag_col_name in flag_col_names:
+        if flag_col_name not in df.columns:
+            df = df.with_columns(pl.lit(0).alias(flag_col_name))
+    return df
+
+
+def add_unchecked_flag(df: pl.DataFrame, flag_col_names: list) -> pl.DataFrame:
     """
     Add "unchecked" flag to all values in flag column
 
@@ -42,26 +66,32 @@ def add_unchecked_flag(df: pl.DataFrame, flag_col_name: str) -> pl.DataFrame:
 
     Returns:
         Updated dataframe
-
     """
-    return df.with_columns((pl.col(flag_col_name) + core_flag_config["unchecked"].id).alias(flag_col_name))
+    flag_val = core_flag_config["unchecked"].id
+
+    for flag_col_name in flag_col_names:
+        df = df.with_columns((pl.col(flag_col_name) + flag_val).alias(flag_col_name))
+    return df
 
 
-def add_missing_flag(df: pl.DataFrame, data_col_name: str, flag_col_name: str) -> pl.DataFrame:
+def add_missing_flag(df: pl.DataFrame, flag_col_dict: dict) -> pl.DataFrame:
     """
     Add a flag for missing values in the specified data column.
 
     Args:
         df: The Polars DataFrame to check and update.
-        data_col_name: The name of the column to check for missing values.
-        flag_col_name: The name of the flag column to add/update with the flag.
+        flag_col_dict: Dictionary of names of the data column and flag column.
 
     Returns:
         A DataFrame with the flag column updated for missing values in the specified data column.
     """
-    return df.with_columns(
-        pl.when(pl.col(data_col_name).is_null() | pl.col(data_col_name).is_nan())
-        .then(pl.col(flag_col_name) + core_flag_config["missing"].id)
-        .otherwise(pl.col(flag_col_name))
-        .alias(flag_col_name)
-    )
+    flag_val = core_flag_config["missing"].id
+
+    for data_col_name, flag_col_name in flag_col_dict.items():
+        df = df.with_columns(
+            pl.when(pl.col(data_col_name).is_null() | pl.col(data_col_name).is_nan())
+            .then(pl.col(flag_col_name) + flag_val)
+            .otherwise(pl.col(flag_col_name))
+            .alias(flag_col_name)
+        )
+    return df
--- tests/test_flagging.py
+++ tests/test_flagging.py
@@ -3,11 +3,13 @@ from unittest.mock import patch, MagicMock
 
 from datetime import datetime
 import polars as pl
+from parameterized import parameterized
 import numpy as np
 
 from time_series import TimeSeries
 from dritimeseriesprocessor.flagging import (
     initialise_core_flags,
+    add_flag_columns,
     add_unchecked_flag,
     add_missing_flag
 )
@@ -65,14 +67,39 @@ class TestInitialiseCoreFlags(unittest.TestCase):
             self.assertEqual(result.df.columns, ["time"])
 
 
+class TestAddFlagColumns(unittest.TestCase):
+    """Unit tests for the add_flag_columns function."""
+    def setUp(self):
+        """Set up a sample DataFrame for testing."""
+        self.df = pl.DataFrame({
+            "data": [10, 20, 30]
+        })
+
+    @parameterized.expand([
+        ("One column", ["flag1"]),
+        ("multi columns", ["flag1", "flag2", "flag3"]),
+        ("No columns", []),
+    ])
+    def test_add_columns(self, name, col_names):
+        """Test columns are successfully added, set to 0's"""
+        result = add_flag_columns(self.df, col_names)
+        expected_cols = ["data"] + col_names
+        self.assertEqual(result.columns, expected_cols)
+
+        for col_name in col_names:
+            self.assertEqual(result[col_name].to_list(), [0, 0, 0])
+
+
 class TestAddUncheckedFlag(unittest.TestCase):
     """Unit tests for the add_unchecked_flag function."""
 
     def setUp(self):
         """Set up a sample DataFrame for testing."""
         self.df = pl.DataFrame({
-            "data": [10, 20, 30],
-            "flag": [0, 0, 0]  # Initial flag values
+            "data1": [1, 2, 3],
+            "flag1": [0, 0, 0],
+            "data2": [10, 20, 30],
+            "flag2": [0, 0, 0],
         })
 
     def test_add_unchecked_flag(self):
@@ -81,12 +108,14 @@ class TestAddUncheckedFlag(unittest.TestCase):
         with patch.dict('dritimeseriesprocessor.__metadata__.config_core_flags.core_flag_config',
                         mock_core_flag_config, clear=True):
             # Call the function
-            result = add_unchecked_flag(self.df, "flag")
+            result = add_unchecked_flag(self.df, ["flag1", "flag2"])
 
             # Expected result
             expected_df = pl.DataFrame({
-                "data": [10, 20, 30],
-                "flag": [1, 1, 1]  # All values in the flag column should be updated to 1
+                "data1": [1, 2, 3],
+                "flag1": [1, 1, 1],  # All values in the flag column should be updated to 1
+                "data2": [10, 20, 30],
+                "flag2": [1, 1, 1],  # All values in the flag column should be updated to 1
             })
 
             # Check if the result matches the expected DataFrame
@@ -95,7 +124,7 @@ class TestAddUncheckedFlag(unittest.TestCase):
     def test_nonexistent_flag_column(self):
         """Test behavior when the specified flag column does not exist."""
         with self.assertRaises(pl.exceptions.ColumnNotFoundError):
-            add_unchecked_flag(self.df, "nonexistent_flag")
+            add_unchecked_flag(self.df, ["nonexistent_flag"])
 
 
 class TestAddMissingFlag(unittest.TestCase):
@@ -104,9 +133,15 @@ class TestAddMissingFlag(unittest.TestCase):
     def setUp(self):
         """Set up a sample DataFrame for testing."""
         self.df = pl.DataFrame({
-            "data": [10., None, np.nan, float("nan"), 50.],
-            "flag": [0, 0, 0, 0, 0]  # Initial flag values
+            "data1": [1., None, np.nan, float("nan"), 50.],
+            "flag1": [0, 0, 0, 0, 0],
+            "data2": [10., None, np.nan, float("nan"), 50.],
+            "flag2": [0, 0, 0, 0, 0]
         })
+        self.flag_col_dict = {
+            "data1": "flag1",
+            "data2": "flag2",
+        }
 
 
     def test_add_missing_flag(self):
@@ -115,12 +150,14 @@ class TestAddMissingFlag(unittest.TestCase):
         with patch.dict('dritimeseriesprocessor.__metadata__.config_core_flags.core_flag_config',
                         mock_core_flag_config, clear=True):
             # Call the function
-            result = add_missing_flag(self.df, "data", "flag")
+            result = add_missing_flag(self.df, self.flag_col_dict)
 
             # Expected result
             expected_df = pl.DataFrame({
-                "data": [10., None, np.nan, float("nan"), 50.],
-                "flag": [0, 2, 2, 2, 0]  # Flags updated for missing values
+                "data1": [1., None, np.nan, float("nan"), 50.],
+                "flag1": [0, 2, 2, 2, 0],
+                "data2": [10., None, np.nan, float("nan"), 50.],
+                "flag2": [0, 2, 2, 2, 0]
             })
 
             # Check if the result matches the expected DataFrame
@@ -129,23 +166,22 @@ class TestAddMissingFlag(unittest.TestCase):
     def test_no_missing_values(self):
         """Test behavior when there are no missing values in the data column."""
         df_no_missing = pl.DataFrame({
-            "data": [10, 20, 30, 40, 50],
-            "flag": [0, 0, 0, 0, 0]
+            "data1": [1, 2, 3, 4, 5],
+            "flag1": [0, 0, 0, 0, 0],
+            "data2": [10, 20, 30, 40, 50],
+            "flag2": [0, 0, 0, 0, 0]
         })
-        result = add_missing_flag(df_no_missing, "data", "flag")
-
-        # Expected result should be the same as input
-        expected_df = df_no_missing
+        result = add_missing_flag(df_no_missing, self.flag_col_dict)
 
         # Check if the result matches the expected DataFrame
-        self.assertTrue(result.equals(expected_df), "The DataFrames should be equal.")
+        self.assertTrue(result.equals(df_no_missing), "The DataFrames should be equal.")
 
     def test_nonexistent_data_column(self):
         """Test behavior when the specified data column does not exist."""
         with self.assertRaises(pl.exceptions.ColumnNotFoundError):
-            add_missing_flag(self.df, "nonexistent_data", "flag")
+            add_missing_flag(self.df, {"nonexistent_data": "flag1"})
 
     def test_nonexistent_flag_column(self):
         """Test behavior when the specified flag column does not exist."""
         with self.assertRaises(pl.exceptions.ColumnNotFoundError):
-            add_missing_flag(self.df, "data", "nonexistent_flag")
+            add_missing_flag(self.df, {"data1": "nonexistent_flag"})
