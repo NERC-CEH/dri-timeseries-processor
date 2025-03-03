7da6ded6a6bcc5790ae09ae469c213a70a33bc59
Dom Ginger <domgin@ceh.ac.uk>
2025-03-03 09:16:02 +0000

    FW-525 remove temp code and run staging on todays date

--- src/dritimeseriesprocessor/parser.py
+++ src/dritimeseriesprocessor/parser.py
@@ -2,7 +2,6 @@
 
 import argparse
 import datetime
-import random
 from argparse import ArgumentParser
 from datetime import date, timedelta
 from typing import Tuple
@@ -64,27 +63,11 @@ def build_date_range(period: str, end_date: str, environment: str) -> Tuple[str,
     Returns:
         A tuple of the start and end date
     """
-    # TEMP CODE-TO REMOVE FW-525
-    # Level m1 data from the swarm only exists for a 6 month period (Jan-June 2024)
-    # so there will only ever be certain dates stored in the level 0 bucket.
-    # End dates are hardcoded here to make sure the app runs as it would with
-    # live data.
-    # Different datasets will require different hardcoded dates, thus will need
-    # to be updated accordingly.
 
     # When running locally, we just need a date that has been loaded into the
     # `parquet-data` folder.
     if environment == "local":
         end_date = "2024-03-10"
-
-    # For staging, pick a random date from the range of dates in the level 0 bucket.
-    if environment == "staging":
-        swarm_start_date = datetime.datetime(2024, 2, 26)
-        swarm_end_date = datetime.datetime(2024, 6, 6)
-        dates = [swarm_start_date + timedelta(days=x) for x in range((swarm_end_date - swarm_start_date).days)]
-
-        # Pick random date
-        end_date = datetime.datetime.strftime(random.choice(dates), "%Y-%m-%d")
 
     end_date = validate_end_date(end_date)
 
