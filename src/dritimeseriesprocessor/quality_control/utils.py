192138124d4e2382dbd4dd756425eb88ac5cb4ea
ricsmi <ricsmi@ceh.ac.uk>
2025-04-29 15:54:49 +0100

    Remove references to old QC stuff

--- src/dritimeseriesprocessor/quality_control/utils.py
+++ src/dritimeseriesprocessor/quality_control/utils.py
@@ -4,8 +4,6 @@ from typing import List, Union
 import polars as pl
 from time_stream import TimeSeries
 
-from dritimeseriesprocessor.__metadata__.config_quality_control import get_qc_config
-
 logger = logging.getLogger(__name__)
 
 
@@ -66,102 +64,6 @@ def column_threshold_check(
     ts.add_flag(flag_column, flag_id, operator_expr)
 
     return ts
-
-
-def get_site_spike_threshold(site_id: str, variable: str, resolution: str) -> float:
-    """Get the site-specific spike threshold values for a given site, column and resolution.
-
-    Args:
-        site_id: Site ID for which spike threshold is needed
-        variable: The variable name for which spike threshold is needed
-        resolution: The temporal resolution for which spike threshold is needed
-
-    Returns:
-        Spike threshold
-    """
-    spike_thresholds = get_qc_config("spike_thresholds")
-    spike_threshold = spike_thresholds.get(variable)
-    if spike_threshold is None:
-        raise UserWarning(f"No {variable} spike thresholds provided.")
-
-    # Get the default values for the given resolution
-    default_spike_threshold_value = next(
-        (
-            spike_thresh
-            for spike_thresh in spike_threshold.defaults
-            if spike_thresh.resolutions is None or resolution in spike_thresh.resolutions
-        ),
-        None,
-    )
-
-    # Get the site specific values for the given site and resolution, defaulting to default values if not found
-    if spike_threshold.sites:
-        spike_threshold_value = next(
-            (
-                spike_thresh
-                for spike_thresh in spike_threshold.sites
-                if spike_thresh.site_id == site_id
-                and (spike_thresh.resolutions is None or resolution in spike_thresh.resolutions)
-            ),
-            default_spike_threshold_value,
-        )
-    else:
-        spike_threshold_value = default_spike_threshold_value
-
-    if spike_threshold_value is None:
-        raise ValueError(f"No spike threshold set for {variable} spike test")
-
-    return spike_threshold_value.threshold
-
-
-def get_site_range_values(site_id: str, variable: str, resolution: str) -> tuple[float, float]:
-    """Get the site-specific minimum and maximum values for range check for a given site, column and resolution.
-
-    This function searches through the site-specific range thresholds and returns the min and max values for
-    a given site and resolution. If no specific values are found, it returns the default values for that column
-    and resolution.
-
-    Args:
-        site_id: Site ID for which range values are needed
-        variable: The variable name for which range values are needed
-        resolution: The temporal resolution for which range values are needed
-
-    Returns:
-        A tuple containing the minimum and maximum values for the range check.
-    """
-    range_thresholds = get_qc_config("range_thresholds")
-    range_threshold = range_thresholds.get(variable)
-    if range_threshold is None:
-        raise UserWarning(f"No {variable} range thresholds provided.")
-
-    # Get the default values for the given resolution
-    default_range_values = next(
-        (
-            range_thresh
-            for range_thresh in range_threshold.defaults
-            if range_thresh.resolutions is None or resolution in range_thresh.resolutions
-        ),
-        None,
-    )
-
-    # Get the site specific values for the given site and resolution, defaulting to default values if not found
-    if range_threshold.sites:
-        range_values = next(
-            (
-                range_thresh
-                for range_thresh in range_threshold.sites
-                if range_thresh.site_id == site_id
-                and (range_thresh.resolutions is None or resolution in range_thresh.resolutions)
-            ),
-            default_range_values,
-        )
-    else:
-        range_values = default_range_values
-
-    if range_values is None:
-        raise ValueError(f"No min/max values set for {variable} range test")
-
-    return range_values.min_value, range_values.max_value
 
 
 def get_failed_qc_check_ids_from_flag(flag: int) -> List[int]:
