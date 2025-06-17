846029ef2c7944be49cfec17976c1b8a6a7f853d
Simon Stanley <simsta@ceh.ac.uk>
2025-06-17 14:07:27 +0100

    Add qc check tests

--- tests/dritimeseriesprocessor/quality_control/test_checks.py
+++ tests/dritimeseriesprocessor/quality_control/test_checks.py
@@ -4,8 +4,16 @@ from datetime import datetime
 import polars as pl
 from time_stream import TimeSeries
 
-from dritimeseriesprocessor.quality_control.checks import (battery_voltage_check, error_codes_check, range_check,
-                                                           soilmet_scans_check, spike_check)
+from dritimeseriesprocessor.quality_control.checks import (
+    battery_voltage_check,
+    error_codes_check,
+    range_check,
+    soilmet_scans_check,
+    spike_check,
+    pluvio_diagnostic_check,
+    snow_distance_signal_check,
+    tdt_soil_temp_check
+)
 
 
 class TestBatteryVoltageCheck(unittest.TestCase):
@@ -252,3 +260,157 @@ class TestSpikeCheck(unittest.TestCase):
 
         result = spike_check(self.ts, "value1", "value1_QC_FLAG", self.flag_name, 10.)
         self.assertEqual(result.df['value1_QC_FLAG'].to_list(), [0, 1, 0, 0, 1, 0])
+
+
+class TestRadiometerTaCheck(unittest.TestCase):
+    # Place holder
+    pass
+
+
+class TestHeatFluxPlateCheck(unittest.TestCase):
+    # Place holder
+    pass
+
+
+class TestPluvioDiagnosticCheck(unittest.TestCase):
+    """Test pluvio_diagnostic_check function."""
+
+    def setUp(self):
+        self.flag_name = "precip_diag"
+
+        data = pl.DataFrame({
+            'time': [
+                datetime(2023, 8, 10),
+                datetime(2023, 8, 11),
+                datetime(2023, 8, 12),
+                datetime(2023, 8, 13),
+                datetime(2023, 8, 14),
+            ],
+            "PRECIP_DIAG": [0., 0., 1., 20., -1],
+            "value": [1., 2., 3., 4., 5.],
+        })
+
+        self.ts = TimeSeries(data, "time")
+        self.ts.add_flag_system("qc_flags", {self.flag_name: 1})
+        self.ts.init_flag_column("qc_flags", "value_QC_FLAG")
+
+    def test_pluvio_diag_below_threshold(self):
+        """ Test that correct flags applied to values that match where PRECIP_DIAG is above threshold
+        """
+        result = pluvio_diagnostic_check(self.ts, "value", "value_QC_FLAG", self.flag_name, 0., "")
+        self.assertEqual(result.df['value_QC_FLAG'].to_list(), [0, 0, 1, 1, 0])
+
+    def test_pluvio_diag_none_below_threshold(self):
+        """ Test that all flags applied because precip diag values all above threshold
+        """
+        result = pluvio_diagnostic_check(self.ts, "value", "value_QC_FLAG", self.flag_name, -10., "")
+        self.assertEqual(result.df['value_QC_FLAG'].to_list(), [1, 1, 1, 1, 1])
+
+    def test_pluvio_diag_all_below_threshold(self):
+        """ Test that no flags applied to all values because precip diag values all below threshold
+        """
+        result = pluvio_diagnostic_check(self.ts, "value", "value_QC_FLAG", self.flag_name, 100., "")
+        self.assertEqual(result.df['value_QC_FLAG'].to_list(), [0, 0, 0, 0, 0])
+
+    def test_no_precip_diag_column(self):
+        """ Test terror raised when 'PRECIP_DIAG' column is missing.
+        """
+        self.ts.df = self.ts.df.drop(["PRECIP_DIAG"])
+        with self.assertRaises(UserWarning):
+            pluvio_diagnostic_check(self.ts, "value", "value_QC_FLAG", self.flag_name, 0., "")
+
+
+class TestSnowDistanceSignalCheck(unittest.TestCase):
+    """Test snow_distance_signal_check function."""
+
+    def setUp(self):
+        self.flag_name = "snowd_signal"
+
+        data = pl.DataFrame({
+            'time': [
+                datetime(2023, 8, 10),
+                datetime(2023, 8, 11),
+                datetime(2023, 8, 12),
+                datetime(2023, 8, 13),
+                datetime(2023, 8, 14),
+            ],
+            "SNOWD_SIGNALQUALITY": [160., 170., 1., 20., -1],
+            "value": [1., 2., 3., 4., 5.],
+        })
+
+        self.ts = TimeSeries(data, "time")
+        self.ts.add_flag_system("qc_flags", {self.flag_name: 1})
+        self.ts.init_flag_column("qc_flags", "value_QC_FLAG")
+
+    def test_snowd_signal_below_threshold(self):
+        """ Test that correct flags applied to values that match where SNOWD_SIGNALQUALITY is above threshold
+        """
+        result = snow_distance_signal_check(self.ts, "value", "value_QC_FLAG", self.flag_name, 152., "")
+        self.assertEqual(result.df['value_QC_FLAG'].to_list(), [0, 0, 1, 1, 1])
+
+    def test_snowd_signal_none_below_threshold(self):
+        """ Test that no flags applied because snowd signal all above threshold
+        """
+        result = snow_distance_signal_check(self.ts, "value", "value_QC_FLAG", self.flag_name, -10., "")
+        self.assertEqual(result.df['value_QC_FLAG'].to_list(), [0, 0, 0, 0, 0])
+
+    def test_snowd_signal_all_below_threshold(self):
+        """ Test that flags applied to all values because snowd signal all below threshold
+        """
+        result = snow_distance_signal_check(self.ts, "value", "value_QC_FLAG", self.flag_name, 300., "")
+        self.assertEqual(result.df['value_QC_FLAG'].to_list(), [1, 1, 1, 1, 1])
+
+    def test_no_snowd_signal_column(self):
+        """ Test terror raised when 'SNOWD_SIGNALQUALITY' column is missing.
+        """
+        self.ts.df = self.ts.df.drop(["SNOWD_SIGNALQUALITY"])
+        with self.assertRaises(UserWarning):
+            snow_distance_signal_check(self.ts, "value", "value_QC_FLAG", self.flag_name, 0., "")
+
+
+class TestTDTSoilTempCheck(unittest.TestCase):
+    """Test tdt_soil_temp_check function."""
+
+    def setUp(self):
+        self.flag_name = "tdt_tsoil"
+
+        data = pl.DataFrame({
+            'time': [
+                datetime(2023, 8, 10),
+                datetime(2023, 8, 11),
+                datetime(2023, 8, 12),
+                datetime(2023, 8, 13),
+                datetime(2023, 8, 14),
+            ],
+            "TDT1_TSOIL": [0.1, 0.5, 1., 1.5, 2.1],
+            "TDT1_VWC": [41., 52., 43., 54., 45.],
+        })
+
+        self.ts = TimeSeries(data, "time")
+        self.ts.add_flag_system("qc_flags", {self.flag_name: 1})
+        self.ts.init_flag_column("qc_flags", "TDT1_VWC_QC_FLAG")
+
+    def test_tdt_tsoil_below_threshold(self):
+        """ Test that correct flags applied to values that match where TDT1_TSOIL is above threshold
+        """
+        result = tdt_soil_temp_check(self.ts, "TDT1_VWC", "TDT1_VWC_QC_FLAG", self.flag_name, 1., "")
+        self.assertEqual(result.df['TDT1_VWC_QC_FLAG'].to_list(), [1, 1, 0, 0, 0])
+
+    def test_tdt_tsoil_none_below_threshold(self):
+        """ Test that no flags applied because tsoil all above threshold
+        """
+        result = tdt_soil_temp_check(self.ts, "TDT1_VWC", "TDT1_VWC_QC_FLAG", self.flag_name, 0., "")
+        self.assertEqual(result.df['TDT1_VWC_QC_FLAG'].to_list(), [0, 0, 0, 0, 0])
+
+    def test_tdt_tsoil_all_below_threshold(self):
+        """ Test that flags applied to all TDT1_VWCs because tsoil all below threshold
+        """
+        result = tdt_soil_temp_check(self.ts, "TDT1_VWC", "TDT1_VWC_QC_FLAG", self.flag_name, 5., "")
+        self.assertEqual(result.df['TDT1_VWC_QC_FLAG'].to_list(), [1, 1, 1, 1, 1])
+
+    def test_no_precip_diag_column(self):
+        """ Test terror raised when 'TDT1_TSOIL' column is missing.
+        """
+        self.ts.df = self.ts.df.drop(["TDT1_TSOIL"])
+        with self.assertRaises(UserWarning):
+            tdt_soil_temp_check(self.ts, "TDT1_TSOIL", "TDT1_VWC_QC_FLAG", self.flag_name, 0., "")
