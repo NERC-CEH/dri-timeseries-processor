0d351f46b72c5f9d81e089d055939481851b8c9c
Simon Stanley <simsta@ceh.ac.uk>
2025-06-20 17:30:13 +0100

    Update tests with no more ts grouping

--- tests/dritimeseriesprocessor/preprocessing/test_preprocessor.py
+++ tests/dritimeseriesprocessor/preprocessing/test_preprocessor.py
@@ -5,17 +5,8 @@ import polars as pl
 from polars.testing import assert_frame_equal
 
 from time_stream import TimeSeries, Period
-from dritimeseriesprocessor.preprocessing.preprocessor import pr_flag_column_name, run_preprocess
-
-
-class TestPRFlagColumnName(unittest.TestCase):
-    """Unit tests for the pr_flag_column_name function.
-    """
-    def test_standard_column_name(self):
-        """
-        Test that the function correctly appends '_PR_FLAG' to a standard column name.
-        """
-        self.assertEqual(pr_flag_column_name('data'), 'data_PR_FLAG')
+from dritimeseriesprocessor.flagging.flagger import add_initial_core_flags
+from dritimeseriesprocessor.preprocessing.preprocessor import run_preprocess
 
 
 class TestRunPreprocessing(unittest.TestCase):
@@ -27,7 +18,7 @@ class TestRunPreprocessing(unittest.TestCase):
         """
         Set up common test data and mocks.
         """
-        data = pl.DataFrame({
+        ta_data = pl.DataFrame({
             'time': [
                 datetime(2023, 8, 10),
                 datetime(2023, 8, 11),
@@ -35,19 +26,63 @@ class TestRunPreprocessing(unittest.TestCase):
                 datetime(2023, 8, 13),
                 datetime(2023, 8, 14),
             ],
-            "SITE_ID": ["site1"] * 5,
-            'temperature': [20.0, 22.0, 21.0, 20.0, 19.0],
+            'temperature': [20.0, 22.0, 21.0, 20.0, 19.0]
+        })
+        rh_data = pl.DataFrame({
+            'time': [
+                datetime(2023, 8, 10),
+                datetime(2023, 8, 11),
+                datetime(2023, 8, 12),
+                datetime(2023, 8, 13),
+                datetime(2023, 8, 14),
+            ],
             'humidity': [50, 55, None, None, 60]
         })
 
         resolution = Period.of_days(1)
         periodicity = Period.of_days(1)
-        self.ts = TimeSeries(
-            data,
+
+        ta_ts = TimeSeries(
+            ta_data,
             "time",
             resolution,
-            periodicity
+            periodicity,
+            metadata={"site_id": "site1"}
         )
+        ta_ts = add_initial_core_flags(ta_ts)
+
+        rh_ts = TimeSeries(
+            rh_data,
+            "time",
+            resolution,
+            periodicity,
+            metadata={"site_id": "site1"}
+        )
+        rh_ts = add_initial_core_flags(rh_ts)
+
+        self.ta_ts_id = "site1_temperature_1d_raw"
+        self.rh_ts_id = "site1_humidity_1d_raw"
+
+        self.ts_ids = {
+            self.ta_ts_id: {
+                "data": ta_ts,
+                # "sourceSite": "site1",
+                # "sourceColumnName": "temperature",
+                # "sourceDataset": "dataset1",
+                # "sourceBucket": "bucket1",
+                # "resolution": resolution,
+                # "periodicity": periodicity,
+            },
+            self.rh_ts_id: {
+                "data": rh_ts,
+                # "sourceSite": "site1",
+                # "sourceColumnName": "humidity",
+                # "sourceDataset": "dataset1",
+                # "sourceBucket": "bucket1",
+                # "resolution": resolution,
+                # "periodicity": periodicity,
+            }
+        }
 
         self.mock_methods = [
             Mock(
@@ -79,16 +114,16 @@ class TestRunPreprocessing(unittest.TestCase):
         mock_preprocessing_config.correction_methods = self.mock_methods
         mock_preprocessing_config.corrections = correction_config
 
-        result = run_preprocess(self.ts)
+        result = run_preprocess(self.ts_ids)
 
         # Check flag system added
-        self.assertIn('pr_flags', result.flag_systems)
+        self.assertIn('pr_flags', result[self.ta_ts_id]["data"].flag_systems)
         # Check columns added
-        self.assertIn('temperature_PR_FLAG', result.columns)
+        self.assertIn('temperature_PR_FLAG', result[self.ta_ts_id]["data"].columns)
         # Check the correction method has been applied
-        self.assertEqual(result.df['temperature'].to_list(), [20.0, 32.0, 31.0, 30.0, 19.0])
+        self.assertEqual(result[self.ta_ts_id]["data"].df['temperature'].to_list(), [20.0, 32.0, 31.0, 30.0, 19.0])
         # Check flag values have been added
-        self.assertEqual(result.df['temperature_PR_FLAG'].to_list(), [0, 1, 1, 1, 0])
+        self.assertEqual(result[self.ta_ts_id]["data"].df['temperature_PR_FLAG'].to_list(), [0, 1, 1, 1, 0])
 
     @patch("dritimeseriesprocessor.preprocessing.preprocessor.preprocessing_config")
     def test_run_preprocessing_no_config(self, mock_preprocessing_config):
@@ -99,9 +134,9 @@ class TestRunPreprocessing(unittest.TestCase):
         mock_preprocessing_config.correction_methods = self.mock_methods
         mock_preprocessing_config.corrections = []
 
-        result = run_preprocess(self.ts)
+        result = run_preprocess(self.ts_ids)
 
-        assert_frame_equal(result.df, self.ts.df)
+        self.assertEqual(result, self.ts_ids)
 
     @patch("dritimeseriesprocessor.preprocessing.preprocessor.preprocessing_config")
     def test_run_preprocessing_no_methods(self, mock_preprocessing_config):
@@ -121,9 +156,9 @@ class TestRunPreprocessing(unittest.TestCase):
         mock_preprocessing_config.correction_methods = []
         mock_preprocessing_config.corrections = [correction_config]
 
-        result = run_preprocess(self.ts)
+        result = run_preprocess(self.ts_ids)
 
-        assert_frame_equal(result.df, self.ts.df)
+        self.assertEqual(result, self.ts_ids)
 
     @patch("dritimeseriesprocessor.preprocessing.preprocessor.preprocessing_config")
     def test_run_preprocessing_multiple_methods(self, mock_preprocessing_config):
@@ -153,12 +188,12 @@ class TestRunPreprocessing(unittest.TestCase):
         mock_preprocessing_config.correction_methods = self.mock_methods
         mock_preprocessing_config.corrections = correction_config
 
-        result = run_preprocess(self.ts)
+        result = run_preprocess(self.ts_ids)
 
         # Check the correction method has been applied
-        self.assertEqual(result.df['temperature'].to_list(), [40.0, 64.0, 62.0, 30.0, 19.0])
+        self.assertEqual(result[self.ta_ts_id]["data"].df['temperature'].to_list(), [40.0, 64.0, 62.0, 30.0, 19.0])
         # Check flag values have been added
-        self.assertEqual(result.df['temperature_PR_FLAG'].to_list(), [2, 3, 3, 1, 0])
+        self.assertEqual(result[self.ta_ts_id]["data"].df['temperature_PR_FLAG'].to_list(), [2, 3, 3, 1, 0])
 
     @patch("dritimeseriesprocessor.preprocessing.preprocessor.preprocessing_config")
     def test_run_preprocessing_end_date_is_none(self, mock_preprocessing_config):
@@ -178,9 +213,9 @@ class TestRunPreprocessing(unittest.TestCase):
         mock_preprocessing_config.correction_methods = self.mock_methods
         mock_preprocessing_config.corrections = correction_config
 
-        result = run_preprocess(self.ts)
+        result = run_preprocess(self.ts_ids)
 
         # Check the correction method has been applied
-        self.assertEqual(result.df['temperature'].to_list(), [20.0, 32.0, 31.0, 30.0, 29.0])
+        self.assertEqual(result[self.ta_ts_id]["data"].df['temperature'].to_list(), [20.0, 32.0, 31.0, 30.0, 29.0])
         # Check flag values have been added
-        self.assertEqual(result.df['temperature_PR_FLAG'].to_list(), [0, 1, 1, 1, 1])
+        self.assertEqual(result[self.ta_ts_id]["data"].df['temperature_PR_FLAG'].to_list(), [0, 1, 1, 1, 1])
--- tests/dritimeseriesprocessor/quality_control/test_utils.py
+++ tests/dritimeseriesprocessor/quality_control/test_utils.py
@@ -9,7 +9,7 @@ from dritimeseriesprocessor.quality_control.utils import column_threshold_check
 
 class TestColumnThresholdCheck(unittest.TestCase):
     def setUp(self):
-        data = pl.DataFrame({
+        a_data = pl.DataFrame({
             'time': [
                 datetime(2023, 8, 10),
                 datetime(2023, 8, 11),
@@ -17,89 +17,101 @@ class TestColumnThresholdCheck(unittest.TestCase):
                 datetime(2023, 8, 13),
             ],
             "value_a": [5., 10., 20., 30.],
+        })
+
+        b_data = pl.DataFrame({
+            'time': [
+                datetime(2023, 8, 10),
+                datetime(2023, 8, 11),
+                datetime(2023, 8, 12),
+                datetime(2023, 8, 13),
+            ],
             "value_b": [1.0, 1.1, 1.2, 1.3],
+        })
+
+        c_data = pl.DataFrame({
+            'time': [
+                datetime(2023, 8, 10),
+                datetime(2023, 8, 11),
+                datetime(2023, 8, 12),
+                datetime(2023, 8, 13),
+            ],
             "value_c": [None, 50., 100., None],
         })
 
         resolution = Period.of_days(1)
         periodicity = Period.of_days(1)
-        self.ts = TimeSeries(
-            data,
-            "time",
-            resolution,
-            periodicity
-        )
 
-        self.ts.add_flag_system("qc_flags", {
-            "TEST": 1
-        })
-        self.ts.init_flag_column("qc_flags", "value_b_QC_FLAG")
-        self.ts.init_flag_column("qc_flags", "value_c_QC_FLAG")
+        self.ts_a = TimeSeries(a_data, "time", resolution, periodicity, metadata={"column_name": "value_a"})
+        self.ts_b = TimeSeries(b_data, "time", resolution, periodicity, metadata={"column_name": "value_b"})
+        self.ts_c = TimeSeries(c_data, "time", resolution, periodicity, metadata={"column_name": "value_c"})
+
+        self.ts_a.add_flag_system("qc_flags", {"TEST": 1})
+        self.ts_b.add_flag_system("qc_flags", {"TEST": 1})
+        self.ts_c.add_flag_system("qc_flags", {"TEST": 1})
+
+        self.ts_b.init_flag_column("qc_flags", "value_b_QC_FLAG")
+        self.ts_c.init_flag_column("qc_flags", "value_c_QC_FLAG")
+
 
     def test_greater_than(self):
         """ Test the column threshold check function with '>' operator.
         """
-        result = column_threshold_check(self.ts, "value_a", "value_b_QC_FLAG", 10, ">", 1)
+        result = column_threshold_check(self.ts_b, self.ts_a, "value_b_QC_FLAG", 10, ">", 1)
         self.assertEqual(result.df['value_b_QC_FLAG'].to_list(), [0, 0, 1, 1])
 
     def test_greater_than_or_equal(self):
         """ Test the column threshold check function with '>=' operator.
         """
-        result = column_threshold_check(self.ts, "value_a", "value_b_QC_FLAG", 10, ">=", 1)
+        result = column_threshold_check(self.ts_b, self.ts_a, "value_b_QC_FLAG", 10, ">=", 1)
         self.assertEqual(result.df['value_b_QC_FLAG'].to_list(), [0, 1, 1, 1])
 
     def test_less_than(self):
         """ Test the column threshold check function with '<' operator.
         """
-        result = column_threshold_check(self.ts, "value_a", "value_b_QC_FLAG", 10, "<", 1)
+        result = column_threshold_check(self.ts_b, self.ts_a, "value_b_QC_FLAG", 10, "<", 1)
         self.assertEqual(result.df['value_b_QC_FLAG'].to_list(), [1, 0, 0, 0])
 
     def test_less_than_or_equal(self):
         """ Test the column threshold check function with '<=' operator.
         """
-        result = column_threshold_check(self.ts, "value_a", "value_b_QC_FLAG", 10, "<=", 1)
+        result = column_threshold_check(self.ts_b, self.ts_a, "value_b_QC_FLAG", 10, "<=", 1)
         self.assertEqual(result.df['value_b_QC_FLAG'].to_list(), [1, 1, 0, 0])
         
     def test_equal(self):
         """ Test the column threshold check function with '==' operator.
         """
-        result = column_threshold_check(self.ts, "value_a", "value_b_QC_FLAG", 10, "==", 1)
+        result = column_threshold_check(self.ts_b, self.ts_a, "value_b_QC_FLAG", 10, "==", 1)
         self.assertEqual(result.df['value_b_QC_FLAG'].to_list(), [0, 1, 0, 0])
         
     def test_not_equal(self):
         """ Test the column threshold check function with '!=' operator.
         """
-        result = column_threshold_check(self.ts, "value_a", "value_b_QC_FLAG", 10, "!=", 1)
+        result = column_threshold_check(self.ts_b, self.ts_a, "value_b_QC_FLAG", 10, "!=", 1)
         self.assertEqual(result.df['value_b_QC_FLAG'].to_list(), [1, 0, 1, 1])
 
     def test_flag_na_when_true(self):
         """ Test that setting flag_na to True means that any NULL values in the check column are treated as failing
         the QC check (so qc flag set in result)
         """
-        result = column_threshold_check(self.ts, "value_c", "value_b_QC_FLAG", 10, ">", 1, flag_na=True)
+        result = column_threshold_check(self.ts_b, self.ts_c, "value_b_QC_FLAG", 10, ">", 1, flag_na=True)
         self.assertEqual(result.df['value_b_QC_FLAG'].to_list(), [1, 1, 1, 1])
 
     def test_flag_na_when_false(self):
         """ Test that setting flag_na to False means that any NULL values in the check column are ignored in
         the QC check (so qc flag not set in result)
         """
-        result = column_threshold_check(self.ts, "value_c", "value_b_QC_FLAG", 10, ">", 1, flag_na=False)
+        result = column_threshold_check(self.ts_b, self.ts_c, "value_b_QC_FLAG", 10, ">", 1, flag_na=False)
         self.assertEqual(result.df['value_b_QC_FLAG'].to_list(), [0, 1, 1, 0])
-
-    def test_missing_check_column(self):
-        """ Test that a missing check column raises error
-        """
-        with self.assertRaises(UserWarning):
-            column_threshold_check(self.ts, "missing_check_column", "value_b_QC_FLAG", 10, ">", 1)
 
     def test_missing_flag_column(self):
         """ Test that a missing qc column raises error
         """
         with self.assertRaises(UserWarning):
-            column_threshold_check(self.ts, "value_a", "missing_QC_FLAG", 10, ">", 1)
+            column_threshold_check(self.ts_b, self.ts_a, "missing_QC_FLAG", 10, ">", 1)
 
     def test_invalid_operator(self):
         """ Test that invalid operator raises error
         """
         with self.assertRaises(ValueError):
-            column_threshold_check(self.ts, "value_a", "value_b_QC_FLAG", 10, ">>", 1)
+            column_threshold_check(self.ts_b, self.ts_a, "value_b_QC_FLAG", 10, ">>", 1)
