47cdd1a9f0e35b33bc29822a508963c4615fe504
Samantha Hewkin <samhew@ceh.ac.uk>
2025-07-25 11:22:31 +0100

    Re-integrate derivations

--- src/dritimeseriesprocessor/time_series_processor.py
+++ src/dritimeseriesprocessor/time_series_processor.py
@@ -7,6 +7,7 @@ from time_stream import TimeSeries
 
 from dritimeseriesprocessor import parser
 from dritimeseriesprocessor.configuration import app_config
+from dritimeseriesprocessor.deriving.process_derivations import DerivationProcessor
 from dritimeseriesprocessor.flagging.flagger import add_initial_core_flags
 from dritimeseriesprocessor.logger import setup_logging
 from dritimeseriesprocessor.metrics_exporter import metrics
@@ -79,7 +80,12 @@ class TimeSeriesProcessor:
         logger.info("Processing data")
         self._process_data()
 
-        # TODO Aggregations and derivations here
+        # TODO Aggregations here
+
+        # Create derived data
+        # -------------------
+        derivation_processor = DerivationProcessor(self.ts_ids)
+        self.ts_ids = derivation_processor.run()
 
         # Writing
         # -------
