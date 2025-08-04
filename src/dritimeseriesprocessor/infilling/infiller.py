eab6f65e5fb541c79855eee094110a339c29391d
ricsmi <ricsmi@ceh.ac.uk>
2025-08-04 15:26:14 +0100

    Start of refactor for infill routines

--- src/dritimeseriesprocessor/infilling/infiller.py
+++ src/dritimeseriesprocessor/infilling/infiller.py
@@ -50,10 +50,8 @@ def run_infilling(ts_ids: Dict[str, Dict[str, Union[str, TimeSeries]]]) -> Dict[
         if INFILL_FLAG_SYS_NAME not in ts.flag_systems:
             ts.add_flag_system(INFILL_FLAG_SYS_NAME, infill_flags_dict)
 
-        column = list(ts.data_columns.keys())[0]
-
         # Add a flag column for the infill method
-        infill_flag_col = infill_flag_column_name(column)
+        infill_flag_col = infill_flag_column_name(ts.column_name)
         if infill_flag_col not in ts.flag_columns:
             ts.init_flag_column(INFILL_FLAG_SYS_NAME, infill_flag_col)
 
@@ -61,12 +59,33 @@ def run_infilling(ts_ids: Dict[str, Dict[str, Union[str, TimeSeries]]]) -> Dict[
         sorted_infillers = sorted(infill_configs, key=lambda x: x.annotations["data-processing-configuration-priority"])
         for config in sorted_infillers:
             # Run infill methods on time series
-            # TODO: Will have to add in start and end dates so that infilling only applied to specific part of time
-            #  series that config is valid for, based on observationInterval startDate and endDate - see ticket FW-740
-            for method in config.configs:
-                infill_func = infill_methods[method.name]
-                logger.info(f"Infilling {column} with method: {method.name}. Constraints: {method.parameters}")
-                ts = infill_func(ts, column, infill_flag_col, method.name, **method.parameters)
+            for infill_method in config.configs:
+                method_metadata = infill_methods[infill_method.name]
+
+                # TODO: Unsure whether this parameter is actually needed.  Not used in any method currently.
+                infill_method.parameters.pop("window")
+
+                logger.info(
+                    f"Infilling {ts.column_name} with method: {infill_method.name}. "
+                    f"Constraints: {infill_method.parameters}"
+                )
+
+                # To work out which values were infilled
+                null_mask_before = ts.df[ts.column_name].is_null()
+
+                # Do the infilling
+                ts = ts.infill(
+                    method_metadata.function_name,
+                    ts.column_name,
+                    observation_interval=infill_method.observation_interval,
+                    **infill_method.parameters,
+                )
+
+                # Update the flag column with the infill method ID
+                null_mask_after = ts.df[ts.column_name].is_null()
+                #   The values to flag (i.e., the ones that have been infilled) are the ones that were null before
+                #       but aren't now
+                ts.add_flag(infill_flag_col, infill_method.name, null_mask_before.ne(null_mask_after))
 
         ts = update_infill_core_flags(ts)
 
