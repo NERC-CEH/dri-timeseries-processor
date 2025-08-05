767a74dc56f582796c194dfa73fb6aedb4394a50
Samantha Hewkin <samhew@ceh.ac.uk>
2025-08-05 14:05:19 +0100

    Linting

--- src/dritimeseriesprocessor/processor.py
+++ src/dritimeseriesprocessor/processor.py
@@ -9,7 +9,8 @@ from time_stream import TimeSeries
 from dritimeseriesprocessor.correcting.correcter import run_corrections
 from dritimeseriesprocessor.infilling.infiller import run_infilling
 from dritimeseriesprocessor.metrics_exporter import metrics
-from dritimeseriesprocessor.quality_control.quality_controller import run_quality_control
+
+# from dritimeseriesprocessor.quality_control.quality_controller import run_quality_control
 from dritimeseriesprocessor.s3_crud import data_manager
 
 logger = logging.getLogger(__name__)
