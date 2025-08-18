5b32648d269f18ddd121b838faa08f707724f297
Samantha Hewkin <samhew@ceh.ac.uk>
2025-08-18 15:09:09 +0100

    Add pagination support to the Metadata API service

--- testing/tests/dritimeseriesprocessor/test_time_series_processor.py
+++ testing/tests/dritimeseriesprocessor/test_time_series_processor.py
@@ -2,12 +2,13 @@ import datetime
 from unittest import mock
 
 from dritimeseriesprocessor.configuration import app_config
-from dritimeseriesprocessor.time_series_processor import TimeSeriesProcessor
 from dritimeseriesprocessor.s3_crud.write import S3Writer
+from dritimeseriesprocessor.time_series_processor import TimeSeriesProcessor
 from metadata_manager.api_manager import MetadataAPIManager
 from testing.utils.mock_metadata_api import MockMetadataAPI
 from testing.utils.s3_test_helper import S3TestHelper
 from testing.utils.timeseries_test_helper import TimeSeriesTestHelper
+
 
 @mock.patch.object(MetadataAPIManager, "_make_api_call")
 class TestTimeSeriesProcessor(S3TestHelper, TimeSeriesTestHelper):
@@ -127,27 +128,26 @@ class TestTimeSeriesProcessor(S3TestHelper, TimeSeriesTestHelper):
 
         self.compare_ts_ids(expected_ts_ids=expected_ts_ids, actual_ts_ids=ts_processor.ts_ids)
 
-
     def test_write_timeseries(self, mock_api_manager: mock.MagicMock) -> None:
         """Test data is correctly written to the processed bucket."""
-    
+
         mock_api_manager.side_effect = MockMetadataAPI(api_data=self.default_metadata_api_data)
         ts_processor = TimeSeriesProcessor(
-            sites="alic1,bunny,chimn,morly", columns="RN,PA,TA", periodicity="PT30M,PT1M",
-            end_date="2024-03-10", period="P2D", network="cosmos"
+            sites="alic1,bunny,chimn,morly",
+            columns="RN,PA,TA",
+            periodicity="PT30M,PT1M",
+            end_date="2024-03-10",
+            period="P2D",
+            network="cosmos",
         )
         s3_bucket = app_config.processed_bucket
         s3_client = ts_processor.s3_client
-    
+
         # We dont really care about any loading or processing so just using our own generated ts_ids object
         # The processed ts ids contain two different resolutions (PT30M and PT1M) each with two different
         # sites. Within each permutation of resolution and site are multiple columns.
         # This structure ensures all functionality tested.
-        ts_ids = self.load_ts_ids_from_json_file(
-            self.input_dir.joinpath(
-                "write", "processed_ts_ids.json"
-            )
-        )
+        ts_ids = self.load_ts_ids_from_json_file(self.input_dir.joinpath("write", "processed_ts_ids.json"))
         writer = S3Writer(s3_client)
         ts_processor._write_timeseries(ts_ids, s3_bucket, "cosmos", writer)
 
@@ -155,6 +155,4 @@ class TestTimeSeriesProcessor(S3TestHelper, TimeSeriesTestHelper):
         # 1) Check the number of items in the bucket matches the number of
         # expected items
         # 2) loop through the expected outputs and check they match the processor output
-        self._check_expected_parquet_files_exist_in_bucket(
-            self.output_dir.joinpath("write", "full_process"), s3_bucket
-        )
+        self._check_expected_parquet_files_exist_in_bucket(self.output_dir.joinpath("write", "full_process"), s3_bucket)
