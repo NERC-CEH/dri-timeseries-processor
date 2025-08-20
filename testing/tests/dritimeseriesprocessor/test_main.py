906e77c151c8c05960baeaf1691c7e59731363f4
natsha <natsha@ceh.ac.uk>
2025-08-20 10:16:07 +0100

    split out e2e test

--- testing/tests/dritimeseriesprocessor/test_main.py
+++ testing/tests/dritimeseriesprocessor/test_main.py
@@ -15,29 +15,45 @@ logger = logging.getLogger(__name__)
 
 @mock.patch.object(MetadataAPIManager, "_make_api_call")
 class TestMain(S3TestHelper):
-    def test_main(self, mock_api_manager: mock.MagicMock) -> None:
-        """End to end test of the timeseries processor.
+    """End to end test of the timeseries processor.
 
-        Run a CLI based test for the timeseries processor and compare the outputs against a series of expected parquet
-        files.
+    Run a CLI based test for the timeseries processor and compare the outputs against a series of expected parquet
+    files.
 
-        This is designed to test running the dritimeseriesprocessor end to end calling __main__.py from the command line
-        as if it were being run by the user.
+    This is designed to test running the dritimeseriesprocessor end to end calling __main__.py from the command line
+    as if it were being run by the user.
 
-        It is assumed that the structure of the expected outputs will match the storage of the generated outputs on S3
+    It is assumed that the structure of the expected outputs will match the storage of the generated outputs on S3
+    """
 
-        Args:
-            cli_args: List of arguments to provide to the command line when running the test. Each component should be
-                a single item in the list, for example ["--network", "cosmos", "--site", "alic1"] etc.
-            expected_base_dir: Path to the base directory containing the expected data. This should be the equivalent
-                of the output bucket used for generating the data during the test. It is assumed that all folders
-                within this directory when combined match the structure of the s3 used to store the generated data.
-                For example, `expected_base_directory/network/date/site/resolution/data.parquet`
-            output_bucket_name: Name of the output bucket to store any data generated during the test. This will be
-                cleared before the test starts.
-        """
+    def test_main_no_existing_processed_data(self, mock_api_manager: mock.MagicMock) -> None:
+        """Test processing when no existing processed data exists"""
+
+        expected_base_dir=self.output_dir.joinpath("end_to_end", "no_existing")
+        output_bucket_name="ukceh-fdri-staging-timeseries-processed"
+        cli_args=[
+            "--sites", "alic1,bunny",
+            "--columns", "PE,TA",
+            "--periodicity", "PT30M,P1D",
+            "--period", "P2D",
+            "--network", "cosmos"
+        ]
+
+        # create mock api responses
+        api_data = self.create_all_metadata_api_data()
+        mock_api_manager.side_effect = MockMetadataAPI(api_data=api_data)
+
+        # run the processor
+        main(cli_args)
+
+        # check the outputs
+        self._check_expected_parquet_files_exist_in_bucket(expected_base_dir, output_bucket_name)
+
+
+    def test_main_existing_processed_data(self, mock_api_manager: mock.MagicMock) -> None:
+        """Test processing when existing processed data exists"""
         base_input_dir=self.input_dir.joinpath("end_to_end")
-        expected_base_output_dir=self.output_dir.joinpath("end_to_end", "basic")
+        expected_base_output_dir=self.output_dir.joinpath("end_to_end", "existing")
         output_bucket_name="ukceh-fdri-staging-timeseries-processed"
         cli_args=[
             "--sites", "alic1,bunny",
