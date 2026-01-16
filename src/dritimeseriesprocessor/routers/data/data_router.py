7d83f4f25173d09e182ad6ffc50cf37c6ddc5d35
ricsmi <ricsmi@ceh.ac.uk>
2026-01-16 10:22:24 +0000

    Add site to bucket path, remove union_by_name from duckdb query

--- src/dritimeseriesprocessor/routers/data/data_router.py
+++ src/dritimeseriesprocessor/routers/data/data_router.py
@@ -58,14 +58,16 @@ class DuckDBDataRouter(DataRouter):
         partitions = [
             f"{container.network}",
             f"dataset={container.source_dataset}",
-            "site=*",
+            f"site={container.source_site_identifier}",
             "date=*",
         ]
         partitions_str = "/".join(partitions)
         bucket_path = f"s3://{container.source_bucket}/{partitions_str}/data.parquet"
         query = f"""
             SELECT {container.time_column_name}, {container.source_column}
-            FROM read_parquet('{bucket_path}', union_by_name=true)
+            FROM read_parquet(
+                '{bucket_path}', hive_partitioning=true
+            )
             WHERE
                 (date BETWEEN ? AND ?) AND
                 site = ?;
--- tests/dritimeseriesprocessor/routers/data/test_data_router.py
+++ tests/dritimeseriesprocessor/routers/data/test_data_router.py
@@ -44,7 +44,9 @@ class TestDuckDBDataRouter:
 
         expected_query = """
             SELECT a_time, a_column_name
-            FROM read_parquet('s3://a_bucket/a_network/dataset=a_data/site=*/date=*/data.parquet', union_by_name=true)
+            FROM read_parquet(
+                's3://a_bucket/a_network/dataset=a_data/site=A_SITE/date=*/data.parquet', hive_partitioning=true
+            )
             WHERE
                 (date BETWEEN ? AND ?) AND
                 site = ?;
@@ -54,5 +56,5 @@ class TestDuckDBDataRouter:
         call_query, call_params = mock_reader.read.call_args.args
 
         assert_frame_equal(result, TEST_DF)  # Return what the mock_reader returned
-        assert call_query == expected_query
+        assert call_query.strip() == expected_query.strip()
         assert call_params == [start, end, "A_SITE"]
