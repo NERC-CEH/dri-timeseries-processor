951512e0f353e4f6357f7db9977bda2af0826641
lcdurkan <leadur@ceh.ac.uk>
2026-02-02 16:03:42 +0000

    WD aggregation method updated to use TimeStream AngularMean

--- tests/dritimeseriesprocessor/operations/aggregation/test_aggregation_methods.py
+++ tests/dritimeseriesprocessor/operations/aggregation/test_aggregation_methods.py
@@ -7,7 +7,7 @@ import time_stream as ts
 from polars.testing import assert_frame_equal
 
 from dritimeseriesprocessor.models.domain_models.processing_config import ProcessingMethodConfig
-from dritimeseriesprocessor.operations.aggregation.aggregation_methods import WD, Max, Mean, Min, Sum
+from dritimeseriesprocessor.operations.aggregation.aggregation_methods import Max, Mean, Min, Sum
 from utils.data_creation import create_timeframe
 
 
@@ -70,6 +70,19 @@ class TestSum:
         assert_frame_equal(result.df, expected)
 
 
+class WD:
+    def test_angular_mean(self) -> None:
+        """Test that the angular mean, e.g. daily wind direction, aggregation works across the full DataFrame."""
+        tf = create_timeframe(list(range(24 * 3)))
+        config = create_method_config(ts.Period.of_days(1))
+
+        result = WD().run(tf, config)
+        expected = pl.DataFrame(
+            {"time": [datetime(2025, 1, 1), datetime(2025, 1, 2), datetime(2025, 1, 3)], "value": [11.5, 35.5, 59.5]}
+        )
+        assert_frame_equal(result.df.select(["time", "value"]), expected)
+
+
 class TestRounding:
     def test_rounding_0(self) -> None:
         """Check a rounding value of 0 is applied correctly."""
@@ -129,16 +142,3 @@ class TestRounding:
 
         with pytest.raises(OverflowError, match="out of range integral type conversion attempted"):
             Sum().run(tf, config)
-
-
-class TestWD:
-    def test_wd_daily(self) -> None:
-        """Test that the daily wind direction aggregation works across the full DataFrame."""
-        tf = create_timeframe(list(range(24 * 3)))
-        config = create_method_config(ts.Period.of_days(1))
-
-        result = WD().run(tf, config)
-        expected = pl.DataFrame(
-            {"time": [datetime(2025, 1, 1), datetime(2025, 1, 2), datetime(2025, 1, 3)], "value": [11.5, 35.5, 59.5]}
-        )
-        assert_frame_equal(result.df.select(["time", "value"]), expected)
