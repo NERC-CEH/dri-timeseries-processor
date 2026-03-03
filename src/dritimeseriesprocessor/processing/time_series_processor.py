11497e9893939cda1caab8aeaea75581db3528a4
lcdurkan <leadur@ceh.ac.uk>
2026-03-03 14:10:27 +0000

    Review changes

--- src/dritimeseriesprocessor/processing/time_series_processor.py
+++ src/dritimeseriesprocessor/processing/time_series_processor.py
@@ -110,7 +110,6 @@ class TimeSeriesProcessor:
                 self.process_dataset(dataset_id)
             except Exception:
                 self.metrics.failed.inc()
-                logger.exception(f"Processing failed for: {dataset_id}")
                 self.graph.datasets[dataset_id].processed = False
                 logger.info(f"Processing failed tag added to: {dataset_id}")
             else:
@@ -126,7 +125,6 @@ class TimeSeriesProcessor:
         container = self.graph.datasets[dataset_id]
         for dataset in container.all_dependencies():
             if hasattr(self.graph.datasets[dataset], "processed"):
-                logger.info(f"Dependency of: {dataset_id} has processing failed tag")
                 self.graph.datasets[dataset_id].processed = False
                 logger.info(f"Processing failed tag added to: {dataset_id}")
                 break
