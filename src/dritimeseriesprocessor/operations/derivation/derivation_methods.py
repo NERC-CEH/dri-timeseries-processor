29c316f6b83e93381171121ff9e894ca6c8835b0
lcdurkan <leadur@ceh.ac.uk>
2026-03-06 10:33:13 +0000

    Added references to papers

--- src/dritimeseriesprocessor/operations/derivation/derivation_methods.py
+++ src/dritimeseriesprocessor/operations/derivation/derivation_methods.py
@@ -374,10 +374,11 @@ class AbsoluteHumidity(DerivationMethod):
 class AtmosphericPressureFactor(DerivationMethod):
     """Calculate correction factor for atmospheric pressure, PA.
     This factor is used to correct neutron counts.
-    See CRNPy correction factor, Desilets & Zreda, 2003
+    See CRNPy correction factor, Desilets & Zreda, 2003: https://doi.org/10.1016/S0012-821X(02)01088-9
     Uses processed data from atmospheric pressure.
     Barometric attenuation length, L is a site annotation.
     P0: "Arbitrary reference pressure [hPa]: Zreda et al. (2012) HESS" - set to a constant of 1000.0
+    See: https://doi.org/10.5194/hess-16-4079-2012
     """
 
     name = "calc_factor_PA"
