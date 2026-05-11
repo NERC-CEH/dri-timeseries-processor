f3af33ecd6089efed2b722a20bbdf0233ab3fb4e
lcdurkan <leadur@ceh.ac.uk>
2026-05-11 14:13:10 +0100

    Remove additional logic from save step

--- src/dritimeseriesprocessor/operations/derivation/derivation_methods.py
+++ src/dritimeseriesprocessor/operations/derivation/derivation_methods.py
@@ -460,7 +460,7 @@ class Albedo(DerivationMethod):
 class NeutronIntensityFactor(DerivationMethod):
     """Calculate incoming neutron count intensity correction factor using a background reference station.
     See COSMOS supporting documentation.
-    See "COSMOS: the COsmic-ray Soil Moisture Observing System" https://hess.copernicus.org/articles/16/4079/2012/
+    See "COSMOS: the Cosmic-ray Soil Moisture Observing System" https://hess.copernicus.org/articles/16/4079/2012/
     See "Intensity correction factors for a cosmic ray neutron sensor": https://zenodo.org/records/4569062
     GAMMA: Scaling factor to adjust for geomagnetic effects
     REF_C0: Site annotation to account for neutron counts due to site calibration
