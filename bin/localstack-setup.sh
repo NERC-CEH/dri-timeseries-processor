177c28352bda9cca2036d9e2ab141fe111e21d21
richjam <ricsmi@ceh.ac.uk>
2026-05-19 14:51:37 +0100

    Update localstack-setup with new bucket names

--- bin/localstack-setup.sh
+++ bin/localstack-setup.sh
@@ -2,19 +2,19 @@
 echo "Initializing localstack services"
 
 echo "########### Creating level-0 bucket ###########"
-awslocal s3api create-bucket --bucket ukceh-fdri-staging-timeseries-level-0 --region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
+awslocal s3api create-bucket --bucket ukceh-dri-staging-ingested --region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
 
 echo "########### Creating processed bucket ###########"
-awslocal s3api create-bucket --bucket ukceh-fdri-staging-timeseries-processed --region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
+awslocal s3api create-bucket --bucket ukceh-dri-staging-processed --region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
 
 echo "########### Load parquet data into level-0 bucket #########"
 
-awslocal s3 cp /var/lib/localstack/parquet-data/raw/ s3://ukceh-fdri-staging-timeseries-level-0/ --recursive
+awslocal s3 cp /var/lib/localstack/parquet-data/raw/ s3://ukceh-dri-staging-ingested/ --recursive
 
 echo "########### Load parquet data into processed bucket #########"
 
-awslocal s3 cp /var/lib/localstack/parquet-data/processed/ s3://ukceh-fdri-staging-timeseries-processed/ --recursive
+awslocal s3 cp /var/lib/localstack/parquet-data/processed/ s3://ukceh-dri-staging-processed/ --recursive
 
 echo "########### Load flux data into level-0 bucket ###########"
 
-awslocal s3 cp /var/lib/localstack/flux-data/ s3://ukceh-fdri-staging-timeseries-level-0/ --recursive
+awslocal s3 cp /var/lib/localstack/flux-data/ s3://ukceh-dri-staging-ingested/ --recursive
